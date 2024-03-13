2fd36c192d50c8e12a5f3f8cf07d17fb81a0d65a
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2024-03-14 00:42:31 +0100

    setup up user model and migration

---
+++ db/migrate/20240313204308_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email, index: { unique: true }
+      t.string :password_digest
+      t.datetime :verified_at, null: true
+
+      t.timestamps
+    end
+  end
+end
