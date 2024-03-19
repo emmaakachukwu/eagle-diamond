c5ed17f9d9c88323962ca1fdb8d39a3e2f665fca
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2024-03-19 01:22:14 +0100

    package index link in sidenav and dynamic active class using 'current_page?'

--- app/helpers/packages_helper.rb
+++ app/helpers/packages_helper.rb
@@ -4,7 +4,7 @@ module PackagesHelper
   def package_preview_css_class(package, recipient)
     classes = ['message']
     classes << 'message--new' unless recipient.opened?
-    classes << 'message--active' if package.id.to_s == params[:id]
+    classes << 'message--active' if current_page? package_path(package)
     classes.join ' '
   end
 end
