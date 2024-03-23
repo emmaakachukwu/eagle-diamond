e463d70cee273eadb8b32d873d1dbc610327e103
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2024-03-23 16:08:03 +0100

    package pages title

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -3,12 +3,14 @@
 class PackagesController < ApplicationController
   def index
     @packages = current_user.packages
+    @title = 'Packages'
   end
 
   def show
     @packages = current_user.packages
-    @package = current_user.packages.find params[:id]
+    @package = @packages.find params[:id]
     @recipient = @package.recipients.recipient(current_user).first
+    @title = "#{@package.title} | Packages"
 
     render 'packages/index'
   rescue ActiveRecord::RecordNotFound
--- app/controllers/sent_packages_controller.rb
+++ app/controllers/sent_packages_controller.rb
@@ -3,13 +3,15 @@
 class SentPackagesController < ApplicationController
   def index
     @packages = current_user.sent_packages
+    @title = 'Sent Packages'
 
     render 'packages/index'
   end
 
   def show
     @packages = current_user.sent_packages
-    @package = current_user.sent_packages.find params[:id]
+    @package = @packages.find params[:id]
+    @title = "#{@package.title} | Sent Packages"
 
     render 'packages/index'
   rescue ActiveRecord::RecordNotFound
