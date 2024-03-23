f02f6738ef682cc7a4306692481152e1bf2d510d
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2024-03-23 01:56:39 +0100

    decrypt package body for display

--- app/models/package.rb
+++ app/models/package.rb
@@ -10,15 +10,22 @@ class Package < ApplicationRecord
 
   before_create :encrypt_body
 
+  def decrypted_body
+    MessageCrypto.new(encryption_key, hash_key: true).decrypt body
+  end
+
   private
+
+  def encrypt_body
+    self.body = MessageCrypto.new(encryption_key, hash_key: true).encrypt body
+  end
 
   # The encryption key is generated using the following values of a package;
   # - sender id
   # - recipient emails
   #   - emails are used for now as not all recipients are expected to be registered users
   #   - TODO: extend to use recipient's email if recipient is not a registered user
-  def encrypt_body
-    key = "#{sender_id}#{recipients.map(&:user).pluck(:email).join}"
-    self.body = MessageCrypto.new(key, hash_key: true).encrypt body
+  def encryption_key
+    "#{sender_id}#{recipients.map(&:user).pluck(:email).join}"
   end
 end
